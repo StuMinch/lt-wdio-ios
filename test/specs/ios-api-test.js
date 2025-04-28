af8364b818b11369613a0261ec7efb31ac9d017b
Stuart Minchington <stuart@Stuarts-Laptop.local>
2025-04-28 10:31:51 -0700

    updated variable name references

--- test/specs/ios-api-test.js
+++ test/specs/ios-api-test.js
@@ -1,15 +1,15 @@
 describe('Test API Calls', () => {
   
     it('should tap on api calls option', async () => {
-        const alerts = await $('//*[@value="API Calls"]');
-        await alerts.waitForDisplayed({ timeout: 5000 });
-        alerts.click();
+        const apiCalls = await $('//*[@value="API Calls"]');
+        await apiCalls.waitForDisplayed({ timeout: 5000 });
+        apiCalls.click();
       });
     
     it('should tap send 25 get requests button', async () => {
-      const generateAlert = await $('//*[@name="Send 25 GET Requests"]');
-      await generateAlert.waitForDisplayed({ timeout: 5000 });
-      generateAlert.click();
+      const sendRequest = await $('//*[@name="Send 25 GET Requests"]');
+      await sendRequest.waitForDisplayed({ timeout: 5000 });
+      sendRequest.click();
       await driver.pause(10000);
     });
   });
